f9670035a142ad3f62f6f2b38c9f14e41fbd8e2e
Romain Bourré <romain.bourre@me.com>
2020-11-04 16:56:38 +0100

    implement route to get cities by postal code

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Controllers\CitySearchController;
 use App\Controllers\HelloWorldController;
+use App\Domain\Services\Cities\CitiesService;
+use App\Infrastructure\Cities\CitiesRepository;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -12,6 +15,14 @@ $app = AppFactory::create();
 $app->get('/', function (Request $request, Response $response, array $args) {
     $controller = new HelloWorldController();
     return $controller->Index($request, $response, $args);
+});
+
+$app->get("/cities", function (Request $request, Response $response, array $args) {
+    $citiesRepository = new CitiesRepository();
+    $citiesService = new CitiesService($citiesRepository);
+
+    $controller = new CitySearchController($citiesService);
+    return $controller->getCitiesByPostalCode($request, $response, $args);
 });
 
 $app->run();
