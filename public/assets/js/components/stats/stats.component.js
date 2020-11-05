840759de93c0c391e587efe5e27dcae50ab465c5
Hugo Pochart <hugo.pochart@gmail.com>
2020-11-05 13:38:48 +0100

    wip

--- public/assets/js/components/stats/stats.component.js
+++ public/assets/js/components/stats/stats.component.js
@@ -8,6 +8,65 @@ export default class CityStatsContainer {
         this.update();
     }
 
+    newTest(geojson){
+        var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 600 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+        var svg = d3.select('#map').append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+
+        var x = d3.scaleLinear().range([0, 500]);
+            var y = d3.scaleLinear().range([500, 0]);
+        
+        x.domain([0, 50]);
+        y.domain([0, 50]);
+        
+        var point = {"x": 24, "y": 31}
+        
+        var poly = [{"x":10, "y":50},
+                {"x":20,"y":20},
+                {"x":50,"y":10},
+                {"x":30,"y":30}];
+        
+        // console.log("==================================================================================");
+        // console.log(geojson.coordinates)
+        // console.log("==================================================================================");
+        svg.selectAll("polygon")
+            .data(geojson.coordinates)
+        .enter().append("polygon")
+            .attr("points",function(d) { 
+                console.log("==================================================================================");
+                console.log(d)
+                console.log("==================================================================================");
+                return d.map(function(d) {
+                    console.log([x(d[0]),y(d[1])].join(","))
+
+                    return [x(d[0]),y(d[1])].join(",");
+                }).join(" ");
+            });
+        
+        svg.append("circle")
+            .attr("r", 4)
+            .attr("cx", x(point.x))
+            .attr("cy", y(point.y))
+        
+            // add the X Axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        // add the Y Axis
+        svg.append("g")
+            .call(d3.axisLeft(y));
+    }
+
+    
+
     update(city = null) {
         this.city = city;
         this.loadCity(() => {
@@ -43,6 +102,28 @@ export default class CityStatsContainer {
     }
 
     averageData() {
+        console.log(this.cityStats[0]["Geo Shape"]);
+        const frimo = {
+            type: "Polygon",
+            coordinates: [[[2.186690144719194, 48.869027280709375], [2.186720343127883, 48.86892675774901], [2.186787614432625, 48.86618956389059], [2.186907420603295, 48.86602489547758], [2.186742083254524, 48.86604985357489], [2.186656084092235, 48.866057363419614], [2.185504986209498, 48.86304116832668], [2.18341463615303, 48.8624756133261], [2.183397439138298, 48.862442219505766], [2.183273588034174, 48.86251692093491], [2.181860687137993, 48.86334367872085], [2.180464776569315, 48.86412736642945], [2.179398058531254, 48.864692951302295], [2.17934874885903, 48.86470790236218], [2.179301981034295, 48.864734562549884], [2.179270008091639, 48.864773915258986], [2.179262626250835, 48.86480983914981], [2.179272949589076, 48.8648467838196], [2.179294248006098, 48.86487930693539], [2.180281342850003, 48.866342139022855], [2.182123052529826, 48.86755718411669], [2.184248644497048, 48.86840899428386], [2.184451873766759, 48.868489522857416], [2.185049381588601, 48.86872562736165], [2.185611776648615, 48.868940804638285], [2.186170185278823, 48.86914965658412], [2.186235517721356, 48.86915639630057], [2.186311966024082, 48.86914972117342], [2.186357244665478, 48.86913114245347], [2.186620315748868, 48.86904659207977], [2.186690144719194, 48.869027280709375]]]
+        }
+        const geojson = {
+            "type": "FeatureCollection", 
+            "features": [
+                {"geometry": {
+                    "type": "GeometryCollection", 
+                    "geometries": [
+                        frimo
+                    ]
+                }, 
+                "type": "Feature",
+                "properties": {}}
+            ]
+        };
+
+        // JSON.parse(frimo)
+
+        this.newTest(frimo)
         let moyenneInterfaceNumeriques = 0;
         let moyenneAccesInfo = 0;
         let moyenneCompetencesAdmin = 0;
