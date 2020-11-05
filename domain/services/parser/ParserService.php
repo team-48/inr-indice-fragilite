af3ca337281a3ac184834d77acdf7c7f2eb03e5b
Romain Bourré <romain.bourre@me.com>
2020-11-05 17:10:33 +0100

    add all score to stats

--- domain/services/parser/ParserService.php
+++ domain/services/parser/ParserService.php
@@ -10,6 +10,12 @@ class ParserService implements IParserService
     private static int $COLUMN_CITY_CODE = 10;
     private static int $COLUMN_CITY_NAME = 0;
     private static string $COLUMN_CITY_DEPARTMENT_SCORE = "SCORE GLOBAL departement 1";
+    private static string $COLUMN_CITY_INFORMATION_ACCESS_SCORE = "ACCÈS AUX INTERFACES NUMERIQUES departement 1";
+    private static string $COLUMN_CITY_ACCESS_SCORE = "GLOBAL ACCES departement 1";
+    private static string $COLUMN_CITY_ADMINISTRATIVE_SKILLS_SCORE = "COMPETENCES ADMINISTATIVES departement 1";
+    private static string $COLUMN_CITY_DIGITAL_ACCESS_SCORE = "ACCÈS AUX INTERFACES NUMERIQUES departement 1";
+    private static string $COLUMN_CITY_SCHOOL_SKILLS_SCORE = "COMPÉTENCES NUMÉRIQUES / SCOLAIRES departement 1";
+    private static string $COLUMN_CITY_SKILLS_SCORE = "GLOBAL COMPETENCES  departement 1";
 
     private IStatisticsRepository $statisticsRepository;
 
@@ -25,7 +31,8 @@ class ParserService implements IParserService
     /**
      * @inheritdoc
      */
-    public function getCityStatisticsByCityCode(string $cityCode): array {
+    public function getCityStatisticsByCityCode(string $cityCode): array
+    {
         $departmentCode = substr($cityCode, 0, 2);
 
         $cities = $this->statisticsRepository->getCityStatsByDepartment($departmentCode);
@@ -34,7 +41,7 @@ class ParserService implements IParserService
 
         $scoring = $this->computeScoring($headers, $cities);
 
-        $citiesForCityCode =  $this->filterCityByCityCode($headers, $cities, $cityCode);
+        $citiesForCityCode = $this->filterCityByCityCode($headers, $cities, $cityCode);
 
         return [
             'scoring' => $scoring,
@@ -42,7 +49,8 @@ class ParserService implements IParserService
         ];
     }
 
-    private function getColumnIndexByName(array $headers, string $name) {
+    private function getColumnIndexByName(array $headers, string $name)
+    {
         return array_search($name, $headers);
     }
 
@@ -52,11 +60,13 @@ class ParserService implements IParserService
      * @param array $headers
      * @return array
      */
-    private function formatRowWithHeader(array $row, array $headers) {
+    private function formatRowWithHeader(array $row, array $headers)
+    {
         $arr = array();
         $i = 0;
 
-        foreach ($row as $elt) {
+        foreach ($row as $elt)
+        {
             $arr[$headers[$i]] = $elt;
             $i++;
         }
@@ -71,15 +81,19 @@ class ParserService implements IParserService
      * @param string $value
      * @return array
      */
-    private function filterCityByCityCode(array $headers, array $cities, string $value) {
-        if ($value[0] === "0") {
+    private function filterCityByCityCode(array $headers, array $cities, string $value)
+    {
+        if ($value[0] === "0")
+        {
             $value = substr($value, 1);
         }
 
         $filteredCities = array();
 
-        foreach ($cities as $city) {
-            if (strcmp($city[self::$COLUMN_CITY_CODE], $value) === 0 ){
+        foreach ($cities as $city)
+        {
+            if (strcmp($city[self::$COLUMN_CITY_CODE], $value) === 0)
+            {
                 array_push($filteredCities, $this->formatRowWithHeader($city, $headers));
             }
         }
@@ -87,15 +101,66 @@ class ParserService implements IParserService
         return $filteredCities;
     }
 
-    private function computeScoring(array $headers, array $cities) {
+    private function computeScoring(array $headers, array $cities)
+    {
         $result = new CityStatsScoring();
 
-        foreach ($cities as $city) {
+        foreach ($cities as $city)
+        {
             $departmentScoreIndex = $this->getColumnIndexByName($headers, self::$COLUMN_CITY_DEPARTMENT_SCORE);
             $departmentScore = (int)$city[$departmentScoreIndex];
 
-            if ($departmentScore > $result->department) {
+            if ($departmentScore > $result->department)
+            {
                 $result->department = $departmentScore;
+            }
+
+            $informationAccessScoreIndex = $this->getColumnIndexByName($headers, self::$COLUMN_CITY_INFORMATION_ACCESS_SCORE);
+            $informationAccessScore = (int)$city[$informationAccessScoreIndex];
+
+            if ($informationAccessScore > $result->informationAccess)
+            {
+                $result->informationAccess = $informationAccessScore;
+            }
+
+            $accessScoreIndex = $this->getColumnIndexByName($headers, self::$COLUMN_CITY_ACCESS_SCORE);
+            $accessScore = (int)$city[$accessScoreIndex];
+
+            if ($accessScore > $result->access)
+            {
+                $result->access = $accessScore;
+            }
+
+
+            $administrativeSkillsScoreIndex = $this->getColumnIndexByName($headers, self::$COLUMN_CITY_ADMINISTRATIVE_SKILLS_SCORE);
+            $administrativeSkillsScore = (int)$city[$administrativeSkillsScoreIndex];
+
+            if ($administrativeSkillsScore > $result->administrativeSkills)
+            {
+                $result->administrativeSkills = $administrativeSkillsScore;
+            }
+
+            $digitalInterfaceAccessScoreIndex = $this->getColumnIndexByName($headers, self::$COLUMN_CITY_DIGITAL_ACCESS_SCORE);
+            $digitalInterfaceAccessScore = (int)$city[$digitalInterfaceAccessScoreIndex];
+
+            if ($digitalInterfaceAccessScore > $result->digitalInterfacesAccess)
+            {
+                $result->digitalInterfacesAccess = $digitalInterfaceAccessScore;
+            }
+
+            $schoolSkillsScoreIndex = $this->getColumnIndexByName($headers, self::$COLUMN_CITY_SCHOOL_SKILLS_SCORE);
+            $schoolSkillsScore = (int)$city[$schoolSkillsScoreIndex];
+
+            if ($schoolSkillsScore > $result->schoolSkills)
+            {
+                $result->schoolSkills = $schoolSkillsScore;
+            }
+
+            $skillsScoreIndex = $this->getColumnIndexByName($headers, self::$COLUMN_CITY_SKILLS_SCORE);
+            $skillsScore = (int)$city[$skillsScoreIndex];
+
+            if ($skillsScore > $result->skills) {
+                $result->skills = $skillsScore;
             }
         }
 
